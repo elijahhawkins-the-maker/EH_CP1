16e520542f4b9564cab90c7140da77efad877c5c
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-08-25 09:09:51 -0600

    bruh 4

--- starters/seasons.py
+++
@@ -1,7 +0,0 @@
-#EH_seasons
-last_name = input("yo what is your last name:",    )
-input(last_name)
-print("yo cool")
-print("yo what is your favorite season:",     )
-input()
-print(last_name, "i like winter too")
