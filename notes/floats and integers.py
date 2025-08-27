263badc7fec4d02d0811c0aa13d9f193e496ca5a
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-08-27 09:52:58 -0600

    bruh 4

---
+++ notes/floats and integers.py
@@ -0,0 +1,41 @@
+#EH 1st integers and floats notes
+
+#integers
+age = 999999999999999999999999
+year = 2025
+room = 200
+hour = 9
+motivation = -69420
+
+#float (floating point number)
+pi= 3.141592653589793238634662
+percent = .75
+damage = 9.75
+
+#math
+print(5+2) #7
+print(5-2) #3
+print(5*2) #10
+print(5/2) #2.5
+print(5//2) #2
+print(5%2) #1
+print(5**2) #25
+
+print(5+2 *10 + (5**5)%3 -50 * 1000.5 / 156)
+
+#type casting
+num = int(3.141592653589793238634) #does not round
+decimal = float(8)
+print(num)
+print(decimal)
+
+#using variables
+hours = 3
+minutes = 4
+
+print(hours*60+minutes)
+
+#to change a user input data type you have to surround the 
+age = input("how old are you: \n")
+
+print("wow you old old as fudge!",525600*age, "minutes!")
