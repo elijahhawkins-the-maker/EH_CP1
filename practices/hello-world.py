a37aaaadedadc4fb2c9f81d0cb149c708ba8ae39
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-08-28 19:24:37 -0600

    bruh 6

--- practices/hello-world.py
+++ practices/hello-world.py
@@ -1,5 +1,3 @@
 #EH 1st, hello world
 name = input("what is your name:  ")
-print("what is your name")
-name = input()
-print("hello", name)
+print("hello,", name)
--- projects/my_grades.py
+++ projects/my_grades.py
@@ -3,14 +3,17 @@ grade_1 = int(input("what is your grade in your 1st class:   "))
 
 grade_2 = int(input("what is your grade in your 2nd class:     "))
 
-grade_3 = int("what is your grade in your 3rd class:   ")
+grade_3 = int(input("what is your grade in your 3rd class:   "))
 
-grade_4 = int("what is your grade in your 4th class:    ")
+grade_4 = int(input("what is your grade in your 4th class:    "))
 
-grade_5 = int("what is your grade in your 5th class:   ")
+grade_5 = int(input("what is your grade in your 5th class:   "))
 
-grade_6 = int("what is your grade for your 6th class:   ")
+grade_6 = int(input("what is your grade in your 6th class:   "))
 
-grade_7 = int("and finally what is your grade for your 7th class,    ")
+grade_7 = int(input("and finally what is your grade for your 7th class:   "))
 
-float(grade_1 +  grade_2 + grade_3 + grade_4 + grade_5 + grade_6 + grade_7)
+total_grade = grade_1 + grade_2 + grade_3 + grade_4 + grade_5 + grade_6 + grade_7
+total_grade2 = (total_grade/7)
+total_grade3 = round(total_grade2, 2)
+print("your average grade is", total_grade3)
