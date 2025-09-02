eeee6521b58f906f0ccd8383182374bac50d1fc4
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-02 09:52:57 -0600

    bruh 8

--- notes/string notes.py
+++ notes/string notes.py
@@ -1,1 +1,31 @@
-#EH 1st period strings notes
+#EH 1st strings notes
+
+#what is a string
+sentence = "the quick brown fox jumps over the lazy dog!"
+
+first_name = "bruh"
+month = "september"
+movie = "the revenge of the sith"
+food = "chocolate"
+
+print(sentence)
+#get length of string
+print(len(sentence))
+length = len(sentence)
+print("the sentence is", length, "characters long")
+
+
+print('"the true meaning of smekday" is the best book ever')
+#escape characters
+print("\'the true meaning of smekday\' is the best book\never")
+
+
+#concatenate
+last_name = "hawkins"
+full_name = first_name + "Elijah"
+
+print(full_name)
+
+#index
+print(last_name[0:4])
+print(sentence[10:15])
---
+++ projects/madlib.py
@@ -0,0 +1,11 @@
+#EH_madlib 1st
+print("Try to make a funny story by entering in some words!")
+
+Name = input("Enter your name (or a random name , ex: ryan reynolds):     ")
+place = input("Enter a random place (like the mall or walmart):    ")
+item = input("Enter a random item:     ")
+verb = input("enter a random verb (such as walking but more funny):   ")
+item2 = input("enter a second item:     ")
+car = input("enter a car:    ")
+incident = input("enter an incident (my car crashed! or something):   ")
+
