7b15880b4de7c9ff31b25c0a286f43d987e22c0c
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-03 09:53:36 -0600

    bruh9

--- projects/madlib.py
+++ projects/madlib.py
@@ -1,11 +1,27 @@
 #EH_madlib 1st
 print("Try to make a funny story by entering in some words!")
 
-Name = input("Enter your name (or a random name , ex: ryan reynolds):     ")
-place = input("Enter a random place (like the mall or walmart):    ")
-item = input("Enter a random item:     ")
-verb = input("enter a random verb (such as walking but more funny):   ")
-item2 = input("enter a second item:     ")
-car = input("enter a car:    ")
-incident = input("enter an incident (my car crashed! or something):   ")
+Name = input("Enter your name (or a random name , ex: ryan reynolds):\n     ")
+place = input("Enter a random place (like the mall or walmart):\n    ")
+isle = input("enter in a section in the place (like a chips isle or something):\n   ")
+item = input("Enter a random item:\n     ")
+verb = input("enter a random verb (such as 'walk' but more funny):\n   ")
+item2 = input("enter a second item:\n     ")
+car = input("enter a car:\n    ")
+incident = input("enter an incident (my car crashed! or something):\n  ")
 
+story = " once upon a time, a guy or gal named " + (Name) + " decided to " + (verb) + " to " + (place) + " to get " + (item) + " but once they went into the " + (place) + "," + " they remembered that they needed to also get, " + (item2) + "!" + " so they " + (verb) + " over to the " + (isle)
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(story)
