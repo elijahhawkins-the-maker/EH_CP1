1ce13ea99d2bc859ad747a86b4a35b42ab3f0a72
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-05 09:53:13 -0600

    bruh15

--- projects/Idiot proof.py
+++ projects/Idiot proof.py
@@ -1,1 +1,12 @@
-#EH idiot proof
+#EH 1st idiot proof
+gpa = float(input("What is your gpa?   "))
+fname = input("what is your first name?    ")
+lname = input("what is your last name?    ")
+fphone = int(input("enter the first three digits of your phone number: "))
+sphone = int(input("enter the second three digits of your phone number:    "))
+
+
+round(gpa, 1)
+
+
+print(fname + " " + lname)
