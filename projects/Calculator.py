76eb091f509ab2a784c794e34b32cad43534e8d2
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-08 20:39:37 -0600

    bruh20!

--- projects/Calculator.py
+++ projects/Calculator.py
@@ -1,5 +1,41 @@
 #EH period 1 basic calculator
+print("Yo here is a fully functioning calculator!!!")
 
-e = input("What equation would you like the calculator to solve?\n add for addtion\n sub for subtration\n m for multiplication\n d for division\n mod for modulo\n int for integer division\n ex for exponents\n ")
+e = input("What equation would you like the calculator to solve?\n add for addtion\n sub for subtraction\n m for multiplication\n d for division\n mod for modulo\n int for integer division\n ex for exponents\n ")
 if e == ("add"):
-    
+    add = float(input("alrighty then what is the first number:\n     "))
+    add2 = float(input("alrighty now what is the second number:\n    "))
+    add3 = float(add + add2)
+    print(add, "+", add2, "=", add3)
+elif e == ("sub"):
+    sub = float(input("alrighty what is the first number:\n    "))
+    sub2 = float(input("alrighty now what is the second number:\n   "))
+    sub3 = float(sub - sub2)
+    print(sub, "-", sub2, "=", sub3)
+elif e == ("m"):
+    mul = float(input("alrighty what is the first number:\n    "))
+    mul2 = float(input("alrighty what is the second number:\n    "))
+    mul3 = float(mul * mul2)
+    print(mul, "*", mul2, "=", mul3)
+elif e == ("d"):
+    div = float(input("alrighty what is the first number:\n    "))
+    div2 = float(input("alrighty what is the second number:\n    "))
+    div3 = float(div / div2)
+    print(div, "/", div2, "=", div3)
+elif e == ("mod"):
+    mod = float(input("alrighty what is the first number:\n    "))
+    mod2 = float(input("alrighty what is the second number:\n    "))
+    mod3 = float(mod % mod2)
+    print(mod, "%", mod2, "=", mod3)
+elif e == ("int"):
+    int = float(input("alrighty what is the first number:\n    "))
+    int2 = float(input("alrighty what is the second number:\n    "))
+    int3 = float(int // int2)
+    print(int, "//", int2, "=", int3)
+elif e == ("ex"):
+    ex = float(input("alrighty what is the first number:\n    "))
+    ex2 = float(input("alrighty what is the second number:\n    "))
+    ex3 = float(ex ** ex2)
+    print(ex, "**", ex2, "=", ex3)
+
+print("Yo your problem should now be solved!".title())
