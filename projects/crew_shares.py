948b2d081fa57057e0bda3e208cab1a390aa35f7
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-15 18:27:28 -0600

    bruh24

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -1,7 +1,8 @@
 #EH 1st crew shares
+print("You can use this to calculate how much money each of your crew members will get")
 money = int(input("how much money did your crew make:\n   ").strip().strip("$"))
 crew = int(input("how many crew members are there:\n   ").strip())
 share = money / (crew + 10)
 captain = share * 7
 first = share * 3
-print(f"the captain gets this much money: ${captain:.2f}\nthe first mate gets this much money: ${first:.2f}\nthis is how much money each crew member gets {(share - 500):.2f}")
+print(f"the captain gets this much money: ${captain:.2f}\nthe first mate gets this much money: ${first:.2f}\nthis is how much money each crew member gets ${(share - 500):.2f}")
