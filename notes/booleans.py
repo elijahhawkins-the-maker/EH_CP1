2254ff615db6ac530c787d898df5613b85e8cf32
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-18 09:52:57 -0600

    bruh27

---
+++ notes/booleans.py
@@ -0,0 +1,29 @@
+#EH 1st Booleans
+import time
+import datetime as date
+
+current_time = time.time()
+readable_time = time.ctime(current_time)
+bruh = date.datetime.now()
+hour = bruh.strftime("%H")
+
+
+print(f"current time in seconds since the date January 1st, 1970(epoch): {current_time}" )
+print(f"today is {readable_time} ")
+print(f"this is the current time: {bruh}")
+print(f"the hour is: {hour}")
+#minutes = %M
+#weekday = %A
+#day = %d
+#month = %B, %b
+#month num = %m
+# year = %Y, %y
+# seconds = %S
+
+print(f"The hour is saved as an integer: {isinstance(hour, int)}")
+print(f"The hour is saved as an float: {isinstance(hour, float)}")
+print(f"The hour is saved as an string: {isinstance(hour, str)}")
+
+over = True
+
+print(f"Hour has a value: {bool(0.000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000067)}")
--- projects/rock_paper_scissors.py
+++ projects/rock_paper_scissors.py
@@ -2,8 +2,8 @@
 import random
 
 rps = input("what do you want to play with (rock, paper, scissors):    ")
-1 = ("rock")
-2 = ("paper")
-3 = ("scissors")
+1 == ("rock")
+2 == ("paper")
+3 == ("scissors")
 if input == ("rock"):
     random.randint;{1,2,3}
