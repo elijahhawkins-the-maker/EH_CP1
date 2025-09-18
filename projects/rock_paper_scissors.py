f320c03222ddcfe354be2910d663bdf292cfcb20
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-18 09:13:31 -0600

    bruh26

--- projects/rock_paper_scissors.py
+++ projects/rock_paper_scissors.py
@@ -1,2 +1,9 @@
 #EH 1st rock paper scissors
-input("what do you want to play with (rock, paper, scissors):    ")
+import random
+
+rps = input("what do you want to play with (rock, paper, scissors):    ")
+1 = ("rock")
+2 = ("paper")
+3 = ("scissors")
+if input == ("rock"):
+    random.randint;{1,2,3}
