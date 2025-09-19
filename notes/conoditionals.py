08a8f1b07f08cad5a9c1a68283b9e69d6581f7fa
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-19 09:52:53 -0600

    bruh29

--- notes/conoditionals.py
+++ notes/conoditionals.py
@@ -12,8 +12,17 @@ monster_defense = 2
 
 hit_roll = random.randint(1,20) + player_attack
 damage_roll = random.randint(1,8) + player_damage
-
-if hit_roll > 12:
+if hit_roll == 20:
+    print("you got a major crit! you get to roll damage")
+    damage_roll = random.randint(1,8) + random.randint(1,8) + player_damage
+    print(f"you did {damage_roll-monster_defense} damage.")
+    monster_hp -= (damage_roll-monster_defense)
+elif hit_roll == 1:
+    print("You rolled what I like to call... a skill issue")
+    damage_roll = random.randint(1,12) + monster_damage
+    player_hp -= (damage_roll - player_defense)
+    print(f"the monster rolled {damage_roll}, your hp is now {player_hp}")
+elif hit_roll + player_attack >= 12:
     print("you hit hard! roll for damage!")
     damage_roll = random.randint(1,8) + player_damage
     if damage_roll > monster_defense:
@@ -23,3 +32,8 @@ if hit_roll > 12:
         print("you did no damage")
 else:
     print("you missed, skill issue.")
+
+print("your turn is over")
+
+if monster_hp > 0:
+    attack_roll = random.randint(1,20) + monster_attack
