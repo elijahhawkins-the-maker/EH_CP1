56fe5c8a82e9b86e5fec77e3dcf62cb7e69a7bd3
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-22 09:51:36 -0600

    bruh30!

---
+++ projects/user_signin.py
@@ -0,0 +1,12 @@
+#EH 1st user sign in
+
+
+u = input("what is your username:   ").strip()
+p = input("What is your password:   ").strip()
+
+if u == "Bruh" or u == "Parker" or u == "GreenDuck5" or u == "imaginary" or u == "meganerd":
+    if p == "emh123" or p == "67676767" or p == "Fred" or p == "3.141592653589793238462662" or p == "nerdyguy" :
+        print("You have signed in successfully, welcome!")
+
+else:
+    print("nah, incorrect password or username,\n bruh")
