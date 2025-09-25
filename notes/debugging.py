966296534cab3774cce0419e9c5163c9105c1a55
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-25 09:53:45 -0600

    bruh34

---
+++ notes/debugging.py
@@ -0,0 +1,51 @@
+#EH 1st fudging debugging
+
+# syntax error - grammar error
+#print"help"
+
+# logic errors - gave the wrong steps
+numone = "2"
+numtwo = "1000"
+
+print(numone + numtwo)
+
+# have a well thought plan
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import time as clock
+
+num = 1
+while True:
+    print(num)
+    clock.sleep(1)
+    num += 1
