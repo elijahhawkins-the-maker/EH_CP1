1efcc02bbc817ea8ca8122669bd602f4014b2546
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-09-26 22:01:06 -0600

    bruh37

--- projects/rock_paper_scissors.py
+++ projects/rock_paper_scissors.py
@@ -1,9 +1,33 @@
 #EH 1st rock paper scissors
 import random
 
-rps = input("what do you want to play with (rock, paper, scissors):    ")
-1 == ("rock")
-2 == ("paper")
-3 == ("scissors")
-if input == ("rock"):
-    random.randint;{1,2,3}
+rps = ["rock", "paper", "scissors"]
+
+print(" I challenge you to rock paper scissors! choose your attack!")
+
+user = input("type your attack below:\n ")
+
+
+if user in rps:
+    rand_choice = random.choice(rps)
+    if rand_choice == "rock" and user == "rock":
+        print(f"You picked {user} and your opponent picked {rand_choice} you tie!")
+    elif rand_choice == "rock" and user == "paper":
+        print(f"You picked {user} and your opponent picked {rand_choice} you won!")
+    elif rand_choice == "rock" and user == "scissors":
+        print(f"You picked {user} and your opponent picked {rand_choice} you lost!")
+    elif rand_choice == "paper" and user == "rock":
+        print(f"You picked {user} and your opponent picked {rand_choice} you lost!")
+    elif rand_choice == "paper" and user == "paper":
+        print(f"You picked {user} and your opponent picked {rand_choice} you tie!")
+    elif rand_choice == "paper" and user == "scissors":
+        print(f"You picked {user} and your opponent picked {rand_choice} you won!")
+    elif rand_choice == "scissors" and user == "rock":
+        print(f"You picked {user} and your opponent picked {rand_choice} you won!")
+    elif rand_choice == "scissors" and user == "paper":
+        print(f"You picked {user} and your opponent picked {rand_choice} you lost!")
+    elif rand_choice == "scissors" and user == "scissors":
+        print(f"You picked {user} and your opponent picked {rand_choice} you tie!")
+else:
+    print("invalid input my guy!")
+
