0180e341692225b69dbb7f4260099b3e147887c4
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-02 19:49:28 -0600

    bruh46

--- projects/multiplication_table.py
+++ projects/multiplication_table.py
@@ -1,5 +1,5 @@
 #EH 1st multiplication table
-num1 = int(input("what is the first number that is in the multiplication table\n"))
-num2 = int(input("what is the last number that is in the multiplication table\n"))
+num1 = int(input("what is the first number in the multiplication table\n"))
+num2 = int(input("what is the last number in the multiplication table\n"))
 for i in range(num1,num2+1):
     print([i*i2 for i2 in range(num1,num2+1)])
