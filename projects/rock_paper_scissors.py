4d74856b147c395eaf4c67600f54dd178b9055ae
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-06 09:48:26 -0600

    bruh51

--- projects/rock_paper_scissors.py
+++ projects/rock_paper_scissors.py
@@ -1,5 +1,11 @@
 #EH 1st rock paper scissors
 import random
+
+s = {
+    "user":0,
+    "computer":0
+}
+
 while True:
     rps = ["rock", "paper", "scissors"]
 
@@ -14,24 +20,31 @@ while True:
             print(f"You picked {user} and your opponent picked {rand_choice} you tie!")
         elif rand_choice == "rock" and user == "paper":
             print(f"You picked {user} and your opponent picked {rand_choice} you won!")
+            s["user"] += 1
         elif rand_choice == "rock" and user == "scissors":
             print(f"You picked {user} and your opponent picked {rand_choice} you lost!")
+            s["computer"] += 1
         elif rand_choice == "paper" and user == "rock":
             print(f"You picked {user} and your opponent picked {rand_choice} you lost!")
+            s["computer"] += 1
         elif rand_choice == "paper" and user == "paper":
             print(f"You picked {user} and your opponent picked {rand_choice} you tie!")
         elif rand_choice == "paper" and user == "scissors":
             print(f"You picked {user} and your opponent picked {rand_choice} you won!")
+            s["user"] += 1
         elif rand_choice == "scissors" and user == "rock":
             print(f"You picked {user} and your opponent picked {rand_choice} you won!")
+            s["user"] += 1
         elif rand_choice == "scissors" and user == "paper":
             print(f"You picked {user} and your opponent picked {rand_choice} you lost!")
+            s["computer"] += 1
         elif rand_choice == "scissors" and user == "scissors":
             print(f"You picked {user} and your opponent picked {rand_choice} you tie!")
-        elif user == "stop": 
-            print("alrighty exiting the program")
-            break
+    elif user == "stop": 
+        print("alrighty exiting the program")
+        break
     else:
         print("invalid input my guy!")
+    print(f"your score is {s["user"]} and your opponents score is {s["computer"]}")
     continue
 
