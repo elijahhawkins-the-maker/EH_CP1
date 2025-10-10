219037c702c62f5e0836367d0cbdcc517a206c16
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-10 09:51:27 -0600

    Bruh56

---
+++ projects/pass_stronk_check.py
@@ -0,0 +1,6 @@
+#EH 1st password strength checker
+p_characters = ["","","","","","",""]
+
+while True:
+    p = input("what is your password that you want to check the strength of?\n")
+#ask user for the password through an input
