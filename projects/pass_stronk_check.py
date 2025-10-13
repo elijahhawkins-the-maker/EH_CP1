1eb1f8431ff3b2232cc314335aa49f75c9df9454
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-13 09:53:26 -0600

    Bruh58

--- projects/pass_stronk_check.py
+++ projects/pass_stronk_check.py
@@ -1,8 +1,21 @@
 #EH 1st password strength checker
-p_characters = ["!","@","#","$","%","^","*","(",")","[","]",",",".","<",">","/","'","|",":",";","_","'"]
-# declare a list with all of the special characters in it
+#print statement to welcome the user
+print("welcome to the password strength checker!")
+p_characters = ["!","@","#","$","%","^","*","(",")","[","]",",",".","<",">","/","'","|",":",";","_","'",'"',"+","=","-","{","}"]
+#declare a list with all of the special characters in it
+#declare a list with all of the numbers in it
+p_numbers = [1,2,3,4,5,6,7,8,9]
+#create a score holder that keeps track of the score
+score = 0
+#make a while loop that replays the code
 while True:
-# make a while loop that re-plays the code
-    p = input("what is your password that you want to check the strength of?\n")
-#ask user for the password through an input
-    if p in p_characters:
+#make a list that will save the amount of points that you scored the try before
+    pro = ["_","_","_","_","_","_","_","_","_"]
+#add a dictionary that keeps track of the specification of the password
+#asks user for the password
+#user input that asks the user for their password
+#if statement that will see if the length is over 12 characters
+#elif statement that checks if the user has special characters
+#elif statement that checks if the user has numbers in their password
+#elif statement that checks if the user has an uppercase letter in their password
+#elif statement that checks if the user has an lowercase letter in their password
