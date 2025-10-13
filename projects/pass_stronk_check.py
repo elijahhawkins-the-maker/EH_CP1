3aea788c99121f3fd629dd9eee90d0d0863580d7
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-13 09:14:43 -0600

    bruh57

--- projects/pass_stronk_check.py
+++ projects/pass_stronk_check.py
@@ -1,6 +1,8 @@
 #EH 1st password strength checker
-p_characters = ["","","","","","",""]
-
+p_characters = ["!","@","#","$","%","^","*","(",")","[","]",",",".","<",">","/","'","|",":",";","_","'"]
+# declare a list with all of the special characters in it
 while True:
+# make a while loop that re-plays the code
     p = input("what is your password that you want to check the strength of?\n")
 #ask user for the password through an input
+    if p in p_characters:
