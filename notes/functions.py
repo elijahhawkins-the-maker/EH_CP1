bcb43a5e0c3c1cd298e328d27f2a35603d7f854c
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-20 09:53:32 -0600

    bruh64

--- notes/functions.py
+++ notes/functions.py
@@ -1,1 +1,22 @@
 #EH 1st funcs notes
+
+def add(x,y):
+    return x+y
+
+total = add(5,5)
+print(total)
+print(f"10 + 72 = {add(10,72)}")
+x = 0
+while x < add(3,9):
+    print("bruh")
+    x += 1
+
+print("bruuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuh")
+
+def initials(name):
+    names = name.split(" ")
+    initials = ""
+    for name in names:
+        initials += name[0]
+
+    return initials
