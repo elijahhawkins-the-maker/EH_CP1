944af103c088f7c18e794b2334713679e4f3007e
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-22 09:53:19 -0600

    bruh67!

--- projects/caesar_cipher.py
+++ projects/caesar_cipher.py
@@ -1,1 +1,15 @@
 #EH 1st caesar cipher
+import re
+upper = r"[A-Z]"
+lower = r"[a-z]"
+while True:
+    def code(text, shift, bruh):
+
+    #user input to see if they would like to 
+    bruh = input("Would you like to decode a message, or encode a message with the caesar cipher?\n")
+    if bruh == "decode":
+        code()
+    elif bruh == "encode":
+        code()
+    else:
+        print("Not a valid input buddy")
