896392e1dc4c2f459301a2200a633ce75f2df045
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-23 09:42:57 -0600

    bruh70!

--- random projects/loading_screen.py
+++ random projects/loading_screen.py
@@ -1,6 +1,9 @@
+import sys
 import time as clock
 
 nums = 2
+
+sys.set_int_max_str_digits(100000)
 
 while True:
 
@@ -8,4 +11,4 @@ while True:
 
     print(nums)
 
-    clock.sleep(0.01)
+    clock.sleep(0.006)
