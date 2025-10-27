55659583128adaded334da764ad0c4ca07d75463
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-27 09:48:53 -0600

    bruh73

--- projects/combat.py
+++ projects/combat.py
@@ -1,19 +1,38 @@
 #EH 1st, dnd combat!
 import random
-health2 = r"[1-20]"
-Chance = r"[1-1000000]"
-while True:
-    def stats(hit_dice, health, armor_class, creatures, atk_bonus):
-        stat = {
-            atk_bonus:0,
-            hit_dice:0,
-            health:0,
-            armor_class:0,
-            "dead":False
-        }
-        name = input("What is your name?\n")
-        creatures = ["Buff Goblin", "You"]
-
-    def user(usr, atk, weapon):
-        weapon = input("What do you want as your weapon?\n You can choose:\nGreat Axe\nSpear\nMusket\or Morningstar")
-        atk = random.randint(health2)
+name = input("What is your name?\n")
+weapon = ["Great Axe","Musket","Spear","Morningstar","mini axe", "javelin"]
+rare_weapon = "Wand of Wonder"
+notdead = True
+while notdead == True:
+    health = 0
+    f_skills = 0
+    s_skills = 0
+    e_skills = 0
+    level = 1
+    damage = 0
+    character = int(input("What is the class that you would like?\n1 for barbarian\n2 for Wizard\n3 for fighter\n4 for engineer\n"))
+    if character == 1:
+        health += random.randint(5,20)
+        f_skills += random.randint(5,20)
+        weapon2 = random.choice(weapon)
+    elif character == 2:
+        health += random.randint(5,20)
+        f_skills += random.randint(5,20) - 4
+        s_skills += random.randint(5,20)
+        weapon2 = random.choice(weapon)
+    elif character == 3:
+        health += random.randint(5,20)
+        f_skills += random.randint(5,20)
+        weapon2 = random.choice(weapon)
+        if f_skills <= 16:
+            f_skills + 4
+    elif character == 4:
+        health += random.randint(5,20)
+        f_skills += random.randint(5,20)
+        weapon2 = random.choice(weapon)
+    else:
+        print("Not a valid input my guy!")
+    print(f"With this character, your health is {health}\nYour fighting skills are {f_skills}\nYour magic skills are {s_skills}\nAnd your weapon is {weapon2}")
+    if weapon2 == "Great Axe":
+        damage += 10
