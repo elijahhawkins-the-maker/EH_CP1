cdab86bda588877569ad6f7064efa30799279eaf
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-29 09:53:34 -0600

    bruh77

--- projects/turtle_race.py
+++ projects/turtle_race.py
@@ -2,6 +2,7 @@
 import random
 import turtle as t
 
+x_cord = 500
 one = t.Turtle()
 two = t.Turtle()
 three = t.Turtle()
@@ -15,11 +16,11 @@ three.penup()
 four.penup()
 five.penup()
 
-one.goto(0,200)
-two.goto(0,100)
-three.goto(0,0)
-four.goto(0,-100)
-five.goto(0,-200)
+one.goto(-300,250)
+two.goto(-300,125)
+three.goto(-300,0)
+four.goto(-300,-125)
+five.goto(-300,-250)
 
 one.pendown()
 two.pendown()
@@ -27,14 +28,51 @@ three.pendown()
 four.pendown()
 five.pendown()
 
+one.speed(3000)
+two.speed(3000)
+three.speed(3000)
+four.speed(3000)
+five.speed(3000)
+
 one.speed(random.randint(1,5))
 two.speed(random.randint(1,5))
 three.speed(random.randint(1,5))
 four.speed(random.randint(1,5))
 five.speed(random.randint(1,5))
 
-for x in range(5):
-    break
+one.shape("turtle")
+two.shape("turtle")
+three.shape("turtle")
+four.shape("turtle")
+five.shape("turtle")
+
+one.color("red")
+two.color("green")
+three.color("blue")
+four.color("purple")
+five.color("yellow")
+
+line = t.Turtle()
+line.penup()
+line.goto(500,500)
+line.pendown()
+line.goto(500,-500)
+line.penup()
+
+for x in range(1,100):
+    one.forward(random.randint(10,100))
+    x1 = one.xcor()
+    two.forward(random.randint(10,100))
+    x2 = two.xcor()
+    three.forward(random.randint(10,100))
+    x3 = three.xcor()
+    four.forward(random.randint(10,100))
+    x4 = four.xcor()
+    five.forward(random.randint(10,100))
+    x5 = five.xcor()
+    continue
+if one.xcor(1) >= x_cord:
+    print("The red turtle won!")
 t.end_fill()
 
 t.done()
