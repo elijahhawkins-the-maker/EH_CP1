5cff0fa8064d4eb6441af4dcd77ac18f13bcac34
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-10-30 09:51:45 -0600

    bruh79

--- projects/turtle_race.py
+++ projects/turtle_race.py
@@ -1,8 +1,9 @@
 #EH 1st turtle racing!
 import random
 import turtle as t
-bruh = t.setup(700,700)
 x_cord = 500
+t.setup(1500,1000)
+t.bgcolor("black")
 one = t.Turtle()
 two = t.Turtle()
 three = t.Turtle()
@@ -16,11 +17,11 @@ three.penup()
 four.penup()
 five.penup()
 
-one.goto(-300,250)
-two.goto(-300,125)
-three.goto(-300,0)
-four.goto(-300,-125)
-five.goto(-300,-250)
+one.goto(-500,250)
+two.goto(-500,125)
+three.goto(-500,0)
+four.goto(-500,-125)
+five.goto(-500,-250)
 
 one.pendown()
 two.pendown()
@@ -53,26 +54,69 @@ four.color("purple")
 five.color("yellow")
 
 line = t.Turtle()
+line.color("white")
 line.penup()
 line.goto(500,500)
 line.pendown()
 line.goto(500,-500)
 line.penup()
+line.hideturtle()
 
-for x in range(1,100):
+for x in range(1,10000):
     one.forward(random.randint(10,100))
+    x1 = one.xcor()
+    if x1 >= 500:
+        red = t.Turtle()
+        red.color("red")
+        red.penup()
+        red.goto(-300,350)
+        red.pendown()
+        red.write("The red turtle wins!",font=("Arial",50,"normal"))
+        red.hideturtle()
+        break
     two.forward(random.randint(10,100))
     x2 = two.xcor()
+    if x2 >= 500:
+        green = t.Turtle()
+        green.color("green")
+        green.penup()
+        green.goto(-300,350)
+        green.pendown()
+        green.write("The green turtle wins!",font=("Arial",50,"normal"))
+        green.hideturtle()
+        break
     three.forward(random.randint(10,100))
     x3 = three.xcor()
+    if x3 >= 500:
+        blue = t.Turtle()
+        blue.color("blue")
+        blue.penup()
+        blue.goto(-300,350)
+        blue.pendown()
+        blue.write("The blue turtle wins!",font=("Arial",50,"normal"))
+        blue.hideturtle()
+        break
     four.forward(random.randint(10,100))
     x4 = four.xcor()
+    if x4 >= 500:
+        purple = t.Turtle()
+        purple.color("purple")
+        purple.penup()
+        purple.goto(-300,350)
+        purple.pendown()
+        purple.write("The purple turtle wins!",font=("Arial",50,"normal"))
+        purple.hideturtle()
+        break
     five.forward(random.randint(10,100))
     x5 = five.xcor()
-    continue
-t.end_fill()
-while True:
-    x1 = one.xcor()
-    if x1 > x_cord:
-        print("The red turtle has won")
+    if x5 >= 500:
+        yellow = t.Turtle()
+        yellow.color("yellow")
+        yellow.penup()
+        yellow.goto(-300,350)
+        yellow.pendown()
+        yellow.write("The yellow turtle wins!",font=("Arial",50,"normal"))
+        yellow.hideturtle()
         break
+t.end_fill()
+t.done()
