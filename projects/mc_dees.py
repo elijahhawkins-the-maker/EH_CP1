f790072c7593ab688aeec32d7ef7c041355c92ac
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-11-12 09:53:38 -0700

    bruh93

--- projects/mc_dees.py
+++ projects/mc_dees.py
@@ -1,31 +1,48 @@
 #EH 1st order up!
 
+import time as t
 main = {
     "burger":9.99,
     "nachos":11.99,
     "burrito":6.99,
-    "extra_crispy_burrito":19.99,
-    "taco_special":15.99,
+    "extra crispy burrito":19.99,
+    "taco special":15.99,
     "quesadilla":4.99,
-    "taco_burger":7.99,
-    "gun_shaped_steak":99.99
+    "taco burger":7.99,
+    "gun shaped steak":99.99
 }
 
 sides = {
     "fries":2.99,
     "extra chips":0.99,
-    "small_burrito":1.99,
-    "small_soda":0.99,
-    "medium_soda":1.99,
-    "large_soda":2.99,
-    "extra_large_soda":3.99
+    "small burrito":1.99,
+    "small soda":0.99,
+    "medium soda":1.99,
+    "large soda":2.99,
+    "extra large_soda":3.99
 }
 des = {
     "ice cream":3.99,
     "bruh":9.99
 }
-
+print("Welcome to McDees, where you always get the best food! What would you like?\n\n")
+t.sleep(3)
+print("These are the main courses!")
 for key in main.keys():
-    if key in main:
-        for i in key:
-            print(main)
+    print(f"{key} is {main[key]}")
+print("\n\n")
+t.sleep(3)
+print("These are the sides!")
+for key in sides.keys():
+    print(f"{key} is {sides[key]}")
+print("\n\n")
+t.sleep(3)
+print("These are the desserts!")
+for key in des.keys():
+    print(f"{key} is {des[key]}")
+print("\n\n")
+bruh = input("Now what main course would you like?")
+bruh2 = input("Now what drink would you like?")
+bruh3 = input("Now what dessert would")
+
+
