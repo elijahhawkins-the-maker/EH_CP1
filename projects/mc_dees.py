5df98fef445ad29003b2f7fc9414623247b7b437
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-11-13 09:52:40 -0700

    bruh94

--- projects/mc_dees.py
+++ projects/mc_dees.py
@@ -1,6 +1,7 @@
 #EH 1st order up!
-
 import time as t
+choices = []
+
 main = {
     "burger":9.99,
     "nachos":11.99,
@@ -41,8 +42,35 @@ print("These are the desserts!")
 for key in des.keys():
     print(f"{key} is {des[key]}")
 print("\n\n")
-bruh = input("Now what main course would you like?")
-bruh2 = input("Now what drink would you like?")
-bruh3 = input("Now what dessert would")
-
-
+while True:
+    bruh = input("Now what main course would you like?\n").lower()
+    if bruh in main:
+        choice = main[bruh]
+        break
+    elif bruh == "none":
+        break
+    else:
+        print("Not an item on the menu")
+while True:
+    bruh2 = input("Now what side would you like?\n").lower()
+    if bruh2 in sides:
+        choice2 = sides[bruh2]
+        break
+    elif bruh == "none":
+        break
+    else:
+        print("Your choice is not on the menu")
+while True:
+    bruh3 = input("Now what dessert would you like?\n").lower()
+    if bruh3 in des:
+        choice3 = des[bruh3]
+        break
+    elif bruh == "none":
+        break
+    else:
+        print("Your choice is not on the menu")
+total = main[bruh] + sides[bruh2] + des[bruh3]
+print(f"Your main course is {bruh} and it is ${choice}")
+print(f"Your side dish is {bruh2} and it is ${choice2}")
+print(f"Your dessert is {bruh3} and it is ${choice3}")
+print(f"Your total is {total} with all of these items")
