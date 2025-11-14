1cc9fe33d98af780083bbdb62e2e21bcb4790197
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2025-11-14 09:43:33 -0700

    bruh97

--- projects/mc_dees.py
+++ projects/mc_dees.py
@@ -31,31 +31,33 @@ des = {
 print("Welcome to McDees, where you always get the best food! What would you like?\n\n")
 #this waits three seconds before it prints the menu
 t.sleep(3)
-#this is a for loop that prints out all of the items in the dictionary in a good way
-print("These are the main courses!")
-for key in main.keys():
-    #prints the key, then the value of that key after it
-    print(f"{key} is ${main[key]}")
-#this separates the menus by 2 lines
-#basically all of the rest of the for loops are the same but with the different dictionaries
-print("\n\n")
-t.sleep(3)
-print("These are the sides and drinks!")
-for key in sides.keys():
-    print(f"{key} is ${sides[key]}")
-print("\n\n")
-t.sleep(3)
-print("These are the desserts!")
-for key in des.keys():
-    print(f"{key} is ${des[key]}")
-print("\n\n")
+#this is a for loop that prints out all of the items in the dictionary in a good way that is inside of a function
+def menu():
+    print("These are the main courses!")
+    for key in main.keys():
+        #prints the key, then the value of that key after it
+        print(f"{key} is ${main[key]}")
+    #this separates the menus by 2 lines
+    #basically all of the rest of the for loops are the same but with the different dictionaries
+    print("\n\n")
+    t.sleep(3)
+    print("These are the sides and drinks!")
+    for key in sides.keys():
+        print(f"{key} is ${sides[key]}")
+    print("\n\n")
+    t.sleep(3)
+    print("These are the desserts!")
+    for key in des.keys():
+        print(f"{key} is ${des[key]}")
+    print("\n\n")
+menu()
 #a while loop that asks the user for the item they want
 while True:
     #the input of the user with the lower() function that switches the input to lowercase
-    bruh = input("Now what main course would you like?\n").lower()
+    main_choice = input("Now what main course would you like?\n").lower()
     #if statement that checks whether the user actually put something that is in the menu
-    if bruh in main:
-        choice = main[bruh]
+    if main_choice in main:
+        choice = main[main_choice]
         #breaks the loop if the if statement is true
         break
     #else statement that tells the user that their choice isn't valid if it wasn't in the menu
@@ -63,31 +65,31 @@ while True:
         print("Not an item on the menu")
 #all of the while loops are the same but with different variables and dictionaries
 while True:
-    bruh2 = input("Now what side would you like?\n").lower()
-    if bruh2 in sides:
-        choice2 = sides[bruh2]
+    side_choice = input("Now what side would you like?\n").lower()
+    if side_choice in sides:
+        choice2 = sides[side_choice]
         break
     else:
         print("Your choice is not on the menu")
 while True:
-    bruh3 = input("Now what is your second side?\n")
-    if bruh3 in sides:
-        choice3 = sides[bruh3]
+    side_choice2 = input("Now what is your second side?\n").lower()
+    if side_choice2 in sides:
+        choice3 = sides[side_choice2]
         break
     else:
         print("Your choice is not on the menu")
 while True:
-    bruh4 = input("Now what dessert would you like?\n").lower()
-    if bruh4 in des:
-        choice4 = des[bruh4]
+    des_choice = input("Now what dessert would you like?\n").lower()
+    if des_choice in des:
+        choice4 = des[des_choice]
         break
     else:
         print("Your choice is not on the menu")
 #this adds all of the choice's pices together for the total
-total = main[bruh] + sides[bruh2] + sides[bruh3] + des[bruh4]
+total = main[main_choice] + sides[side_choice] + sides[side_choice2] + des[des_choice]
 #this set of print statements shows what they ordered and the final price
-print(f"Your main course is {bruh} and it is ${choice}")
-print(f"Your side dish is {bruh2} and it is ${choice2}")
-print(f"Your second side is {bruh3} and it is ${choice3}")
-print(f"Your dessert is {bruh4} and it is ${choice4}")
+print(f"Your main course is {main_choice} and it is ${choice}")
+print(f"Your side dish is {side_choice} and it is ${choice2}")
+print(f"Your second side is {side_choice2} and it is ${choice3}")
+print(f"Your dessert is {des_choice} and it is ${choice4}")
 print(f"Your total is ${total} with all of these items")
