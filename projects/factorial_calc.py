715ddb6b1e32991b5d5f2822874af65c709cec78
elijah.hawkins@ucas-edu.net <elijah.hawkins@ucas-edu.net>
2025-11-21 09:53:14 -0700

    bruh105

---
+++ projects/factorial_calc.py
@@ -0,0 +1,16 @@
+#EHCP1 factorial calculator
+#for num in num
+#def factorial(num-=1)
+#if num == 1
+#print(f"{num} is the og, and {fac} is the factorial)
+#added try and except to idiot proof
+try:
+    number = int(input("What is the number that that you want to check the factorial of\n"))
+except ValueError:
+    print("")
+def fac(factorial):
+    while True:
+        factorial = (number * number - 1)
+        if number == 1:
+            break
+print(factorial)
