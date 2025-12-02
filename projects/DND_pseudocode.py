1e54d00e89d4df7c09757f8ad4d4ca4755107c3c
elijah.hawkins@ucas-edu.net <elijah.hawkins@ucas-edu.net>
2025-12-02 09:52:36 -0700

    bruh109

--- projects/DND_pseudocode.py
+++ projects/DND_pseudocode.py
@@ -1,9 +1,31 @@
 #EHCP1 DND final pseudocode
-#import random
+#import random as r
+#import time as t for time between print statements
 #a print statement that welcomes the adventurer to the country of Draeburg
 #list with the classes in it
-#input that allows them to choose classes for their character such as barbarian, sorcerer, and more
-#if input is in the list
-    #prints the stats for the character
-#else:
-    #prints not a valid character
+#value = 0
+#gold = 50
+#level = 1
+#list for the inventory
+#heal
+#while loop that only executes when the value is under 2
+    #input that allows them to choose classes for their character such as barbarian, sorcerer, and more
+    #strength = r.randint(1,20)
+    #dexterity = r.randint(1,20)
+    #constitution = r.randint(1,20)
+    #intelligence = r.randint(1,20)
+    #wisdom = r.randint(1,20)
+    #charisma = r.randint(1,20)
+    #health = r.randint(10,40)
+    #if statements that check whether they are in a certain number range, then if it is in that number range, it gives a modifier
+    #another user input that asks if they want to roll their stats again
+    #value + 1 to make it so that the while loop can execute one more time
+#functions time!
+#there will be a function for each town, having slight stat changes over each like 
+#there will be a function for combat which will look like this:
+#def combat(health, opponent_health, monster_weapon)
+    #monster hp will get defined
+    #player health will go down or added depending on level and healing potions, healing potions will be defined later
+#print statement that tells the user that they arrived at the first town, Belville
+#yet another function for the shops in the cities, they will mostly have the same stuff, with some small diffs between the two
+#a function will be set for the shop in the poor city and the rich city because of the gold being worth double or half
