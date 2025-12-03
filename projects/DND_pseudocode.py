ce209913f8d71290ad243f6171e70b3b7d84b9cb
elijah.hawkins@ucas-edu.net <elijah.hawkins@ucas-edu.net>
2025-12-03 09:52:41 -0700

    bruh110!

--- projects/DND_pseudocode.py
+++ projects/DND_pseudocode.py
@@ -2,30 +2,61 @@
 #import random as r
 #import time as t for time between print statements
 #a print statement that welcomes the adventurer to the country of Draeburg
+"""-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"""
+#variables:
 #list with the classes in it
+#list with weapons in it
 #value = 0
-#gold = 50
-#level = 1
+#gold will equal 50 to start with
+#level will equal 1 at first
 #list for the inventory
-#heal
+#heal will equal false
+#health
+#max health
+"""-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"""
 #while loop that only executes when the value is under 2
     #input that allows them to choose classes for their character such as barbarian, sorcerer, and more
-    #strength = r.randint(1,20)
-    #dexterity = r.randint(1,20)
-    #constitution = r.randint(1,20)
-    #intelligence = r.randint(1,20)
-    #wisdom = r.randint(1,20)
-    #charisma = r.randint(1,20)
-    #health = r.randint(10,40)
+    #another input for weapons
+    #strength = random integer from 1 to 20
+    #dexterity = random integer from 1 to 20
+    #constitution = random integer from 1 to 20
+    #intelligence = random integer from 1 to 20
+    #wisdom = random integer from 1 to 20
+    #charisma = random integer from 1 to 20
+    #health = random integer from 1 to 20
     #if statements that check whether they are in a certain number range, then if it is in that number range, it gives a modifier
+    #more if statements for 
     #another user input that asks if they want to roll their stats again
     #value + 1 to make it so that the while loop can execute one more time
+    #once value reaches 2, the loop breaks
+"""-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"""
 #functions time!
 #there will be a function for each town, having slight stat changes over each like 
 #there will be a function for combat which will look like this:
-#def combat(health, opponent_health, monster_weapon)
+#define combat(health, opponent_health, weapon, and much more)
     #monster hp will get defined
     #player health will go down or added depending on level and healing potions, healing potions will be defined later
 #print statement that tells the user that they arrived at the first town, Belville
-#yet another function for the shops in the cities, they will mostly have the same stuff, with some small diffs between the two
+#yet another function for the shops in the cities, they will mostly have the same stuff, with some small diffs between them
 #a function will be set for the shop in the poor city and the rich city because of the gold being worth double or half
+#then after all this will be some of the print statements that will tell the user what to do and also the many inputs that will be put for the shops and other things
+"""-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"""
+#what shop functions will look like:
+    #list for the items in the shop, either that or a dictionary with the prices
+    #print statement will be in a for loop
+    #print statement for the items in the shop
+    #time delay in between print statements using the 
+    #there will be a user input for the shop items
+    #conditional that will check whether a item that the user chooses is in the shop (list or dictionary, which ever works better)
+"""----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"""
+#healing function!
+    #once the user gets a healing potion in their inventory, and they use it, there will be a while loop that will loop and add more health until the potion adds 10 more health, or reaches max health
+    #input for the use of the healing potion
+    #while loop will look like this:
+        #adds 1 to health every time it loops
+        #conditional for if the health reaches max or health + 10
+            #then it breaks once it does
+"""----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"""
+#boss function:
+    #will be like combat function but being more hard like stronger attacks and more spells being casted
+    #health will be taken down 1.50x as much because the Dragon King is stronger!
