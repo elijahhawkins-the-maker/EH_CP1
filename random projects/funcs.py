a78361a0d7537924ecacd90f125e1669afb53292
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2026-01-26 10:14:53 -0700

    bruh134

--- random projects/funcs.py
+++ random projects/funcs.py
@@ -1,1 +1,9 @@
 #Helper Functions
+import sys as s
+import time as t
+
+def typing(text, delay=0.03):
+    for char in text:
+        s.stdout.write(char)
+        s.stdout.flush()
+        t.sleep(delay)
