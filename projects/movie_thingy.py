cc28e8a816d85544ea08b9ff7c3134f298a1b7ea
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2026-02-11 10:50:32 -0700

    bruh143

--- projects/movie_thingy.py
+++ projects/movie_thingy.py
@@ -1,31 +1,30 @@
 #EHCP1
 import csv
-import re
-try:
-    with open("EH_CP1\projects\Movies list - Sheet1.csv") as movies:
-        read = csv.reader(movies)
-        header = next(read)
-        content = []
-        for x in read:
-            content.append(
-                {
-                    header[0]: x[0],
-                    header[1]: x[1],
-                    header[2]: x[2],
-                    header[3]: x[3],
-                    header[4]: x[4]
-                }
-            )
-except:
-    print("can't find file")
-else:
-    for movie in content:
-        print(movie)
 
 def find_movie():
-    choice = input("How would you like to find a movie?\nYou can find it by director,\nMovie name\na single letter maybe\nWhatever you want to search it by\n")
-    for x in content:
-        bruh = re.search(content, choice)
-        print(x)
-    print("These are your matches")
+    try:
+        with open("EH_CP1/projects/Movies list - Sheet1.csv", "r") as movies:
+            read = csv.reader(movies)
+            header = next(read)
+            content = []
+            for x in read:
+                content.append(
+                    {
+                        header[i]: x[i] for i in range(len(header))
+                    }
+                )
+    except:
+        print("can't find file")
+    else:
+        pass
+    while True:
+        usr = input("What is the movie that you would like to find?\nSearch anything\n").capitalize()
+        found_movies = [x for x in content if any(usr in str(v) for v in x.values())]
+
+        if found_movies:
+            for movie in found_movies:
+                title = movie.get('Title', "N/A")
+
+        else:
+            print(f"No matches found")
 find_movie()
