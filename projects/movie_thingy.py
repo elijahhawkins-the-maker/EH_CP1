8c0c6c7727cd362570bcbd59a727b2a52a8d2446
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2026-02-12 10:50:12 -0700

    bruh144

--- projects/movie_thingy.py
+++ projects/movie_thingy.py
@@ -1,6 +1,9 @@
-#EHCP1
+#EHCP2
+#import csv at the top
 import csv
-
+#print statement to welcome the user
+print("Welcome to the movie recommender")
+#function for finding the movie
 def find_movie():
     try:
         with open("EH_CP1/projects/Movies list - Sheet1.csv", "r") as movies:
@@ -18,12 +21,26 @@ def find_movie():
     else:
         pass
     while True:
-        usr = input("What is the movie that you would like to find?\nSearch anything\n").capitalize()
-        found_movies = [x for x in content if any(usr in str(v) for v in x.values())]
+        try:
+            usr = int(input("What would you like to do?\n1 for searching for movies\n2 for diplaying the whole list of movies\n3 for exiting\n"))
+            if usr == 1:
+                pass
+            if usr == 2:
+                for i2 in content:
+                    for i3 in i2.values():
+                        print(i3, ", ")
+            if usr == 3:
+                print("exiting...")
+                return
+        except ValueError:
+            print("That ain't something you can do!")
+        usr2 = input("What is the movie that you would like to find?\nSearch anything\n").capitalize()
+        found_movies = [x for x in content if any(usr2 in str(v) for v in x.values())]
 
         if found_movies:
-            for movie in found_movies:
-                title = movie.get('Title', "N/A")
+            for m in found_movies:
+                for i in m.values():
+                    print(i)
 
         else:
             print(f"No matches found")
