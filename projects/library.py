77e4631f9504fe5f899b9aa10f26d2dcebe9eeb8
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2026-02-19 22:51:44 -0700

    bruh142

--- projects/library.py
+++ projects/library.py
@@ -1,75 +1,110 @@
 #EHCP2
 #importing time for delay between print statements
 import time as t
-#original empty list that things will get added to
-library = []
+import csv
+print("Welcome to YOUR personal library!")
+fieldnames = ['title', 'author', 'year', 'genre', 'format', 'rating', 'notes']
+
 def add():
-    usr = input("What do you want to add to your library?\n Please type title and author of book\n").lower()
-    library.append(usr)
-    print(f"You have added '{usr}' to your library")
-    #return menu() at the end of every function to exit and return to menu
-    return menu()
-#this function removes the item from the list with the remove function
-def remov():
-    while True:
-        for x in library:
-            print(x)
-        usr = input("This is what you have!\n what would you like to remove?\nType n to stop removing\n").lower()
-        #some quality idiot proofing with if statements
-        if usr in library:
-            library.remove(usr)
-            break
-        elif usr == "n":
-            return menu()
-        else:
-            print("That ain't in your library!")
-#uses a for loop to effectively search through and find matches through only one word
-def search():
-    while True:
-        usr = input("What is the item you would like to search for?\n Type in author's first name or first word in book title\n").strip().lower()
-        items = [x for x in library if usr in x]
-        #converts found items into a list
-        if items:
-            print("Matches have been found!")
-            for i in items:
-                print(i)
-            return menu()
-        else:
-            print("No matches to your search were found!")
-            return menu()
+    title = input("What is the title of the book that you want to add?\n")
+    author = input("Who is the author of the book?\n")
+    year = int(input("What is the year the book was published?\n"))
+    genre = input("What is the genre of the book?\n")
+    format = input("What is the format\n e.g. poem or normal\n")
+    rating = float(input("What is the rating of the book?\n"))
+    notes = input("Is there any extra notes you want to add?\n")
+    total_inputs = {
+        "title": title,
+        "author": author,
+        "year": year,
+        "genre": genre,
+        "format": format,
+        "rating": rating,
+        "notes": notes
+        }
+    with open("projects/lib.csv", "a", newline = '') as library:
+        writer = csv.DictWriter(library, fieldnames=fieldnames)
+        writer.writerow(total_inputs)
+    print("Now everything has been added to the library")
+    t.sleep(1)
+    return main_menu()
 
 def view():
-    print("These are your items")
-    #converts the list to a tuple
-    lib2 = tuple(library)
-    #for loop to display items
-    for x in lib2:
-        print(x)
-    t.sleep(3)
-    return menu()
-#main menu function with if statements in a while loop and a try-except statement for idiot proofing
-def menu():
-    print("Welcome to YOUR personal library!")
-    while True:
-        try:
-            usr = int(input("What would you like to do?\n 1 for adding to it (do this first if you have nothing in it!)\n 2 for removing something from it\n 3 for finding something in it\n 4 for displaying it\n5 for being done\n"))
-            if usr == 1:
-                add()
-                break
-            if usr == 2:
-                remov()
-                break
-            if usr == 3:
-                search()
-                break
-            if usr == 4:
-                view()
-                break
-            if usr == 5:
-                return
-            else:
-                print("That ain't something you can do!")
-        except ValueError:
-            print("You need to enter a number!")
-menu()
+    print("Alrighty here is what you have")
+    with open("projects/lib.csv", "r") as library:
+        reader = csv.DictReader(library, fieldnames=fieldnames)
+        for row in reader:
+            print(row)
+            t.sleep(1)
+        return main_menu()
+        
 
+def remove():
+    title = input("What is the title of the book that you want to remove?\n")
+    with open("projects/lib.csv", "r") as library:
+        reader = csv.DictReader(library, fieldnames=fieldnames)
+        for row in reader:
+            if row["title"] == title:
+                print(f"Alrighty, you chose to remove {title}")
+                with open("projects/lib.csv", "w", newline = '') as library:
+                    writer = csv.DictWriter(library, fieldnames=fieldnames)
+                    for row in reader:
+                        if row["title"] != title:
+                            writer.writerow(row)
+                            print(f"You successfully removed {title}")
+                            return main_menu()
+
+def update_item():
+    title = input("What is the title of the book that you want to update?\n")
+    with open("projects/lib.csv", "r") as library:
+        reader = csv.DictReader(library, fieldnames=fieldnames)
+        for row in reader:
+            if row["title"] == title:
+                print(f"Alrighty, you chose to update {title}")
+                new_title = input("What is the new title of the book?\n")
+                new_author = input("Who is the new author of the book?\n")
+                new_year = int(input("What is the new year the book was published?\n"))
+                new_genre = input("What is the new genre of the book?\n")
+                new_format = input("What is the new format\n e.g. poem or normal\n")
+                new_rating = float(input("What is the new rating of the book?\n"))
+                new_notes = input("Is there any extra notes you want to add?\n")
+                total_inputs = {
+                    "title": new_title,
+                    "author": new_author,
+                    "year": new_year,
+                    "genre": new_genre,
+                    "format": new_format,
+                    "rating": new_rating,
+                    "notes": new_notes
+                    }
+                with open("projects/lib.csv", "w", newline = '') as library:
+                    writer = csv.DictWriter(library, fieldnames=fieldnames)
+                    for row in reader:
+                        if row["title"] != title:
+                            writer.writerow(row)
+                        else:
+                            writer.writerow(total_inputs)
+                            print(f"You successfully updated {title} to {new_title}")
+                            return main_menu()
+            else:
+                print("That ain't a book in your library")
+                return main_menu()
+
+def main_menu():
+    while True:
+        t.sleep(1)
+        choice = input("What would you like to do?\n1. Add a book\n2. View all books\n3. Remove a book\n4. Update a book\n5. Exit the program\n")
+        if choice == "1":
+            add()
+        elif choice == "2":
+            view()
+        elif choice == "3":
+            remove()
+        elif choice == "4":
+            update_item()
+        elif choice == "5":
+            print("You chose to exit!")
+            return
+        else:
+            print("You can't do that!")
+main_menu()
