03a2ff2d8f49ff89384a0010e1b7e806132776eb
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2026-02-23 23:08:33 -0700

    bruh151

--- projects/word counter/main_wc.py
+++ projects/word counter/main_wc.py
@@ -4,6 +4,7 @@ import time as t
 from file_functions import view_info, view_content, edit_file
 
 def main_menu():
+    print("Welcome to the Document Manager!")
     while True:
         choice = int(input("What would you like to do?\n1. View the info of a txt file\n2. View the file content\n3. Edit the txt file\n4. Exit\n"))
         if choice == 1:
