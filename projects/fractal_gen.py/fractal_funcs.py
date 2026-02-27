9c24f451c5a55e12df25dd533b534f48b8e92171
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2026-02-27 10:52:13 -0700

    bruh155

--- projects/fractal_gen.py/fractal_funcs.py
+++ projects/fractal_gen.py/fractal_funcs.py
@@ -6,14 +6,10 @@ screen = t.Screen()
 t.bgcolor("black")
 
 gen.color("green")
-gen.shape("turtle")
+gen.shape("blank")
 gen.speed(0)
 
 screen.tracer(0)
-
-point_1 = (0,200)
-point_2 = (-200, -100)
-point_3 = (200, -100)
 
 def triangle(point):
     gen.penup()
@@ -27,10 +23,6 @@ def triangle(point):
 def midpoint(p1,p2):
     return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
 
-mid_1 = midpoint(point_1, point_2)
-mid_2 = midpoint(point_2, point_3)
-mid_3 = midpoint(point_1, point_3)
-
 def sierpinski(point, depth):
     triangle(point)
 
@@ -39,10 +31,9 @@ def sierpinski(point, depth):
         sierpinski([point[1], midpoint(point[0], point[1]), midpoint(point[1], point[2])], depth - 1)
         sierpinski([point[2], midpoint(point[2], point[1]), midpoint(point[0], point[2])], depth - 1)
     screen.update()
-    screen.ontimer(sierpinski, 20)
+    screen.ontimer(sierpinski, 0)
 
-triangle_points = [(0,200), (-200,-100), (200,-100)]
+triangle_points = [(0,600), (-600,-300), (600,-300)]
 
-sierpinski(triangle_points, 7)
-gen.hideturtle()
+sierpinski(triangle_points, 8)
 t.done()
---
+++ projects/fractal_gen.py/main.py
@@ -0,0 +1,9 @@
+#EHCP2 main menu
+
+from fractal_funcs import sierpinski
+
+def main():
+    print("Welcome to the sierpinski triangle generator!")
+    choice = input("Now, would you like to generate a cool fractal?\n y or n\n")
+    if choice == "y":
+        
