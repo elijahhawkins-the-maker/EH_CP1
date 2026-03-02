3057bd181ee97b2a131bf1aca12d8feef2e56cfb
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2026-03-02 10:51:05 -0700

    bruh157

--- projects/fractal_gen.py/fractal_funcs.py
+++ projects/fractal_gen.py/fractal_funcs.py
@@ -1,37 +1,32 @@
 import turtle as t
 
-def choices():
-    screen.tracer(0)
-    color = t.textinput("Turtle Color", "What is the color that you would like the triangle to be?\n").lower()
-    if color:
-        t.color(color)
-    background = t.textinput("Background Color", "Now what is the background color that you would like it to be?\n").lower()
-    if background:
-        t.Screen().bgcolor(background)
-    sierpinski(triangle_points, 8)
+from main import choices
 
-gen = t.Turtle()
-screen = t.Screen()
-
-gen.shape("blank")
-gen.speed(0)
-
-screen.tracer(0)
-
-def triangle(point):
-    gen.penup()
-    gen.goto(point[0])
-    gen.pendown()
-    gen.goto(point[1])
-    gen.goto(point[2])
-    gen.goto(point[0])
-
+triangle_points = [(0,600), (-600,-300), (600,-300)]
 
 def midpoint(p1,p2):
     return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2)
 
-t.begin_fill()
-def sierpinski(point, depth):
+def sierpinski(point, depth, background, color):
+
+    gen = t.Turtle()
+    screen = t.Screen()
+
+    gen.shape("blank")
+    gen.color(color)
+    t.bgcolor(background)
+    gen.speed(0)
+
+    screen.tracer(0)
+
+    def triangle(point):
+        gen.penup()
+        gen.goto(point[0])
+        gen.pendown()
+        gen.goto(point[1])
+        gen.goto(point[2])
+        gen.goto(point[0])
+
     triangle(point)
 
     if depth > 0:
@@ -40,8 +35,5 @@ def sierpinski(point, depth):
         sierpinski([point[2], midpoint(point[2], point[1]), midpoint(point[0], point[2])], depth - 1)
     screen.update()
     screen.ontimer(sierpinski, 0)
-
-triangle_points = [(0,600), (-600,-300), (600,-300)]
-t.end_fill()
 
 t.done()
--- projects/fractal_gen.py/main.py
+++ projects/fractal_gen.py/main.py
@@ -1,6 +1,9 @@
 #EHCP2 main menu
 
-from fractal_funcs import choices
+def choices():
+    color = input("What is the color that you would like the triangle to be?\n").lower()
+    background = input("Now what is the background color that you would like it to be?\n").lower()
+    return background, color
 
 def main():
     print("Welcome to the sierpinski triangle generator!")
@@ -8,7 +11,7 @@ def main():
         choice = input("Now, would you like to generate a cool fractal?\n y or n\n")
         if choice == "y":
             choices()
-        elif choice == "y":
+        elif choice == "n":
             break
         else:
             print("That ain't something you can do!")
